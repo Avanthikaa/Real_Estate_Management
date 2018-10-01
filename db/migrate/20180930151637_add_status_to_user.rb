fc2067babce38d0151acadbc8b56269d4ab8a31b
Avanthikaa Ravichandran <avanthikaa96@gmail.com>
2018-10-01 23:35:08 +0530

    Fixing database issues while deployment

--- db/migrate/20180930151637_add_status_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddStatusToUser < ActiveRecord::Migration
-  def change
-    add_column :users, :status, :boolean
-  end
-end
--- db/migrate/20180930165045_remove_status_from_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveStatusFromUsers < ActiveRecord::Migration
-  def change
-    remove_column :users, :users, :boolean
-  end
-end
