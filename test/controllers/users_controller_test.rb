2a182373c904242119066c46072660e381234b08
Avanthikaa Ravichandran <avanthikaa96@gmail.com>
2018-10-16 23:26:27 +0530

    Adding normal tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -6,4 +6,23 @@ class UsersControllerTest < ActionController::TestCase
   #   assert true
   # end
 
+  test "should get index" do
+    get user_index_url
+    assert_response :success
+  end
+
+  test "should not save user without email" do
+    user = User.new
+    user.email = ""
+    assert_not user.save
+  end
+
+  test "should not save user with invalid email" do
+    user = User.new
+    user.name = "xyz"
+    user.email = "xyz.com"
+    user.password = "password"
+    assert_not user.save
+  end
+
 end
